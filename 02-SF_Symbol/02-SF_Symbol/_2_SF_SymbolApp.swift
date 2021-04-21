9c547079d82424259cc40bd60b7238cc1a3d59c5
Fernando Camarillo <camarillo.itsl@gmail.com>
2021-04-20 22:08:51 -0500

    SF Symbol
    Proyecto de imagenes de apple para desarrollo

---
+++ 02-SF_Symbol/02-SF_Symbol/_2_SF_SymbolApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_SF_SymbolApp.swift
+//  02-SF_Symbol
+//
+//  Created by Nando on 20/04/21.
+//
+
+import SwiftUI
+
+@main
+struct _2_SF_SymbolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
