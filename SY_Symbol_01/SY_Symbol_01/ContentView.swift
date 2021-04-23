b918708fffd56bb76df19e4e57de2155d3caee9e
Fernando Camarillo <camarillo.itsl@gmail.com>
2021-04-22 20:44:46 -0500

    modificaciones

--- SY_Symbol_01/SY_Symbol_01/ContentView.swift
+++ SY_Symbol_01/SY_Symbol_01/ContentView.swift
@@ -11,9 +11,37 @@ struct ContentView: View {
     var body: some View {
         
         
-        Image(systemName: "cloud.rain.fill")
-            .foregroundColor(Color.blue)
-            .font(.system(size: 40))
+//        Image(systemName: "cloud.rain.fill")
+//            .foregroundColor(Color.blue)
+//            .font(.system(size: 40))
+        
+        Image("canada")
+            .resizable()
+            .edgesIgnoringSafeArea(.vertical)
+//            .scaledToFill()
+            .aspectRatio(contentMode: .fill)
+//            .clipShape(Circle())
+            .frame(width: 370,height: 370)
+            .opacity(0.8)
+            .overlay(
+//                Image(systemName: "heart.fill")
+//                    .font(.system(size: 60))
+//                    .foregroundColor(.red)
+//                    .opacity(0.7)
+                
+//                Text("Prueba de mensaje con todo los datos para eso, por eso es que yo solo podria estar en casa")
+//                    .fontWeight(.bold)
+//                    .padding()
+//                    .background(Color.gray)
+//                    .cornerRadius(10)
+                
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .opacity(0.4)
+                
+            )
+            
+            
         
     }
 }
