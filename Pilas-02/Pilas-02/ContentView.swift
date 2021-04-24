213933ad8bbb514e454d42625d9fec4e59062e7b
Fernando Camarillo <camarillo.itsl@gmail.com>
2021-04-24 09:06:02 -0500

    pilas
    modificaciones de pilas con vertical , horizotal y profundidad (Z)

--- Pilas-02/Pilas-02/ContentView.swift
+++ Pilas-02/Pilas-02/ContentView.swift
@@ -13,30 +13,48 @@ struct ContentView: View {
             // estracto de codigo mas abajo
             ExtractedView()
             
+            Spacer()
+            
             HStack {
-                VStack{
+                PrecioView(title: "Basico", subTitulo: "curso nuevo", price: "$9.99", textColor: Color.white, fondo: Color.green,icono: "star")
+                
+                ZStack {
+                    PrecioView(title: "Carrera", subTitulo: "curso nuevo", price: "$19.99", textColor: Color.white, fondo: Color.gray,icono: "timer")
                     
-                    Text("Plan basico")
-                        .font(.system(.title))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        
-                    Text("$9.99")
-                        .font(.system(size: 35,weight:.heavy))
-                        .foregroundColor(.white)
+                    Text("Mejor para empezar")
+                        .padding(8)
+                        .font(.system(.caption))
+                        .foregroundColor(.black)
+                        .background(Color.yellow)
+                        .cornerRadius(10)
+                        .offset(x: 0, y: 85)
                     
-                    Text("Un curso nuevo")
-                        .font(.headline)
-                        .foregroundColor(.white)
                     
                 }
-                .padding()
-                .background(Color.green)
-                .cornerRadius(10)
-            }
-        }
+                
+                
+                
+                
+            }.padding(.horizontal)
+         
+            HStack {
+                ZStack {
+                    PrecioView(title: "Definitivo", subTitulo: "curso nuevo", price: "$99.99", textColor: Color.white, fondo: Color.black,icono: "lightbulb")
+                    
+                    Text("Por el mejor precio compralo ya, que esperas!")
+                        .padding(8)
+                        .font(.caption)
+                        .foregroundColor(.black)
+                        .background(Color.yellow)
+                        .cornerRadius(10)
+                        .offset(x: 0, y: 85)
+                    
+                }
+            }.padding()
             
-    
+            Spacer()
+            
+        }
     }
 }
 
@@ -49,11 +67,50 @@ struct ContentView_Previews: PreviewProvider {
 struct ExtractedView: View {
     var body: some View {
         VStack(alignment: .center){
-            Text("Tu itinerario de aprendizaje")
+            Text("Elije tu plan de curso")
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 .font(.system(.largeTitle))
                 .padding()
             
         }
+    }
+}
+
+struct PrecioView: View {
+    
+    //variables
+    var title : String
+    var subTitulo : String
+    var price : String
+    var textColor : Color
+    var fondo : Color
+    var icono : String
+    
+    
+    var body: some View {
+        VStack{
+            
+            Image(systemName: icono)
+                .foregroundColor(textColor)
+                .font(.system(size: 30))
+            
+            Text(title)
+                .font(.system(.title))
+                .fontWeight(.bold)
+                .foregroundColor(textColor)
+            
+            Text(price)
+                .font(.system(size: 34,weight:.heavy))
+                .foregroundColor(textColor)
+            
+            Text(subTitulo)
+                .font(.headline)
+                .foregroundColor(textColor)
+            
+        }
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: 100)
+        .padding(20)
+        .background(fondo)
+        .cornerRadius(10)
     }
 }
